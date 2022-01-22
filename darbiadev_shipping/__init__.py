041ac37d33778e23481476e7b48f2a54e2d984ec
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-01-21 20:16:56 -0600

    fix docs (#1)
    * fix docs
    
    * Pylint fixes
    
    * Add first test

--- darbiadev_shipping/__init__.py
+++ darbiadev_shipping/__init__.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
+"""
+A package wrapping multiple shipping carrier API wrapping packages, providing a higher level multi carrier package.
+"""
 
 from .shipping_services import CarrierEnum, ShippingServices
