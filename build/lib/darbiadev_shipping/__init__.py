6c2bc8d170b24d62f1ae4c3121b129cfaac83212
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-11-12 17:35:01 +0000

    Oh look I committed generated files
    Signed-off-by: GitHub <noreply@github.com>

--- build/lib/darbiadev_shipping/__init__.py
+++
@@ -1,4 +0,0 @@
-"""A package wrapping multiple shipping carrier API wrapping packages, providing a common interface."""
-
-from darbiadev_shipping.shipping_services import ShippingServices
-__all__ = ['ShippingServices']
