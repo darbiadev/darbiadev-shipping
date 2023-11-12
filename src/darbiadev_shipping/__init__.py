d85f96057c8e23ac8167e0f2d5889db2c203d2eb
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-11-12 17:19:50 +0000

    Setup Ruff config
    Signed-off-by: GitHub <noreply@github.com>

--- src/darbiadev_shipping/__init__.py
+++ src/darbiadev_shipping/__init__.py
@@ -1,5 +1,1 @@
-"""
-A package wrapping multiple shipping carrier API wrapping packages, providing a higher level multi carrier package.
-"""
-
-from .shipping_services import CarrierEnum, ShippingServices
+"""A package wrapping multiple shipping carrier API wrapping packages, providing a common interface."""
